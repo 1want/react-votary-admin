8cb4f164066839b659bc45ba26718d5408721db0
1want <iwant1@foxmail.com>
2022-11-15 02:40:13 +0000

    Deploying to gh-pages from  @ 0696c091b93512aa8b07d99924e7dcfc594d9ea4 🚀

---
+++ assets/index.9220297e.js
@@ -0,0 +1,1 @@
+import{j as r,F as a,b as e}from"./index.9c2dec8a.js";const n=()=>r(a,{children:r(e,{type:"primary",children:"btn"})});export{n as default};
--- assets/index.f29ed164.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as a,b as e}from"./index.d7ec5b2c.js";const n=()=>r(a,{children:r(e,{type:"primary",children:"btn"})});export{n as default};
